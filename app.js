42c6145fff07c0d8b6afecbdc8daeb6b5a254fe5
Ral-06 <r4hu106@gmail.com>
2026-05-30 11:25:53 +0530

    Delete (Delete Route)

--- app.js
+++ app.js
@@ -85,3 +85,11 @@ app.put('/listings/:id', async (req, res) => {
   await Listing.findByIdAndUpdate(id, {...req.body.listing});
   res.redirect(`/listings/${id}`);
 });
+//...
+
+// Route to handle deletion of a listing
+app.delete('/listings/:id', async (req, res) => {
+  const {id} = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect('/listings');
+});
