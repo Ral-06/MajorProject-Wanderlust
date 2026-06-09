5980f7da31188184a999833b9a0d9d8a3037c617
Ral-06 <r4hu106@gmail.com>
2026-06-09 21:10:33 +0530

    error.ejs

--- app.js
+++ app.js
@@ -106,7 +106,8 @@ app.use((req, res, next) => {
 // Error handling middleware
 app.use((err, req, res, next) => {
   let{statusCode=500, message="Something went wrong!"} = err;
-  res.status(statusCode).send(message);
+  res.status(statusCode).render('error.ejs', {message});
+  // res.status(statusCode).send(message);
 });
 
 // Listen for incoming requests
