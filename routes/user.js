f50621f4fa14100b177a85797736a92cd3f23e12
Ral-06 <r4hu106@gmail.com>
2026-07-19 19:19:18 +0530

    LogIn after SignUp

--- routes/user.js
+++ routes/user.js
@@ -13,9 +13,16 @@ router.post('/signup', wrapAsync(async (req, res) => {
         let {username, email, password} = req.body;
         const newUser = new User({username, email});
         const registeredUser = await User.register(newUser, password);
-        // console.log(registeredUser);
-        req.flash('success', 'Welcome to Wanderlust!');
-        res.redirect('/listings');
+        // Log the user in after successful registration
+        req.login(registeredUser, (err) => {
+            if(err) {
+                return next(err);
+            }
+            req.flash('success', 'Welcome to Wanderlust!');
+            res.redirect('/listings');
+
+        })
+        //
     } catch (err) {
         req.flash('error', err.message);
         res.redirect('/signup');
