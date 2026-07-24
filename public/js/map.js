d5f0d1b37ac2048fbafc6e189de2290e9a65b8d9
Ral-06 <r4hu106@gmail.com>
2026-07-24 14:25:14 +0530

    Map Marker

--- public/js/map.js
+++ public/js/map.js
@@ -2,6 +2,10 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     accessToken: mapToken,
     container: 'map', // container ID
-    center: [77.2090, 28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9 // starting zoom
 });
+
+const marker = new mapboxgl.Marker({color: "Red"})
+        .setLngLat(coordinates)
+        .addTo(map);
